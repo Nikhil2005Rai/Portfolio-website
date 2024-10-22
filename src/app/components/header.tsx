6b370bf2f421c2cd0330f340da3cb51bcc0a9ba1
Nikhil2005Rai <n2005rai@gmail.com>
2024-10-22 20:14:23 +0530

    Done hamburger menue

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,9 +1,30 @@
+"use client";
 
+import React from "react";
+import NextLink from "next/link";
+import clsx from "clsx";
+import { Link } from "@/lib/types";
+import { useActiveSectionContext } from "@/containers/active-section";
+import { links } from "@/lib/data";
 
-export default function Header() {
+type HeaderProps = { links: Link[] };
+
+export default function Header({ links }: HeaderProps) {
+    const { activeSection, setActiveSection, setTimeOfLastClick } = useActiveSectionContext();
     return (
-        <header>
-            Header
+        <header className="hidden md:flex items-center justify-center fixed z-[999] w-full mt-4">
+            <div>
+                <ul>
+                    {links.map((link) => (
+                        <li>
+                            <NextLink
+                            href={link.hash}>
+                                {link.nameEng}
+                            </NextLink>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </header>
     )
 }
--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -4,11 +4,11 @@ import Header from './header';
 import HamburgerMenu from './hamburger-menu';
 
 export default function Navbar() {
-  return (
-    <nav>
-        <HamburgerMenu links={links} />
-        <Header />
-    </nav>
-  )
+    return (
+        <nav>
+            <HamburgerMenu links={links} />
+            <Header links={links} />
+        </nav>
+    )
 }
 
