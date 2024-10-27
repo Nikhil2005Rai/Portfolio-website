4685a4273b959e1f84ccd5f6c49ad8db629f3b21
Nikhil2005Rai <n2005rai@gmail.com>
2024-10-27 12:51:24 +0530

    Add project section and animations

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,4 +1,4 @@
-import { Link } from "./types"
+import { Link, ProjectInfo } from "./types"
 
 export const links: Link[] = [
     {
@@ -20,5 +20,36 @@ export const links: Link[] = [
     {
         nameEng: "Contact",
         hash: "#contact"
+    },
+]
+
+export const projectsData: ProjectInfo[] = [
+    {
+        title: "Project1",
+        description: "orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type ",
+        tags: ["React", "Next.js", "MongoDB", "Tailwind", "NodeJS"],
+        imageURL: "/image1.png",
+        link: "https://google.com",
+    },
+    {
+        title: "Project2",
+        description: "orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type ",       
+        tags: ["React", "Next.js", "MongoDB", "Tailwind", "NodeJS"],
+        imageURL: "/image2.png",
+        link: "https://google.com",
+    },
+    {
+        title: "Project3",
+        description: "orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type ",        
+        tags: ["React", "Next.js", "MongoDB", "Tailwind", "NodeJS"],
+        imageURL: "/image3.png",
+        link: "https://google.com",
+    },
+    {
+        title: "Project4",
+        description: "orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type ",        
+        tags: ["React", "Next.js", "MongoDB", "Tailwind", "NodeJS"],
+        imageURL: "/image4.png",
+        link: "https://google.com",
     },
 ]
